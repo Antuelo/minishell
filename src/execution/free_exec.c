dddcce629662caf6801a7cdffadf47b42041806d
Antuelo <antuel@outloook.com>
2025-06-20 17:03:31 +0200

    commentaire control_sings

--- src/execution/free_exec.c
+++ src/execution/free_exec.c
@@ -6,7 +6,7 @@
 /*   By: anoviedo <anoviedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 21:49:51 by anoviedo          #+#    #+#             */
-/*   Updated: 2025/06/20 16:50:00 by anoviedo         ###   ########.fr       */
+/*   Updated: 2025/06/20 17:02:19 by anoviedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,21 @@ void	free_envp(char **envp, int count)
 	free(envp);
 }
 
+/*
+| Nom du signal | Numéro | Signification                               | Effet typique                     |
+| ------------- | ------ | ------------------------------------------- | --------------------------------- |
+| `SIGHUP`      | 1      | Hangup (déconnexion du terminal)            | Termine le processus              |
+| `SIGINT`      | 2      | Interruption (Ctrl + C)                     | Termine le processus (130)        |
+| `SIGQUIT`     | 3      | Quitter (Ctrl + \\)                         | Termine + core dump (131)         |
+| `SIGILL`      | 4      | Instruction illégale                        | Termine + core dump               |
+| `SIGABRT`     | 6      | Abandon (abort)                             | Termine + core dump               |
+| `SIGFPE`      | 8      | Erreur arithmétique (ex: division par zéro) | Termine + core dump               |
+| `SIGKILL`     | 9      | Forcer la fin immédiate                     | Termine immédiatement             |
+| `SIGSEGV`     | 11     | Segmentation fault                          | Termine + core dump (139)         |
+| `SIGPIPE`     | 13     | Écriture sur un pipe sans lecteur           | Termine silencieusement           |
+| `SIGALRM`     | 14     | Timeout (alarme)                            | Termine le processus              |
+| `SIGTERM`     | 15     | Demande de terminaison classique            | Termine le processus (par défaut) |
+*/
 int	control_signs(int status, t_exec *exec, int j)
 {
 	int	ss;
@@ -78,39 +93,3 @@ void	wait_all_processes(t_exec *exec)
 		write(1, "\n", 1);
 }
 
-/*
-version de free_envp sans count, mais je l'utilse pas
-pour le moment... on verra...
-void	free_envp_nullterm(char **envp)
-{
-	int	i = 0;
-	while (envp[i])
-		free(envp[i++]);
-	free(envp);
-}*/
-/*void	wait_all_processes(t_exec *exec)
-{
-	int	j;
-	int	status;
-	int	saw_sigint;
-
-	j = 0;
-	saw_sigint = 0;
-	status = 0;
-	while (j < exec->cmd_count)
-	{
-		waitpid(exec->pid[j], &status, 0);
-		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-			saw_sigint = 1;
-		if (j == exec->cmd_count -1)
-		{
-			if (WIFEXITED(status))
-				g_exit_status = WEXITSTATUS(status);
-			else if (WIFSIGNALED(status))
-				g_exit_status = 128 + WTERMSIG(status);
-		}
-		j++;
-	}
-	if (saw_sigint)
-		write(1, "\n", 1);
-}*/
