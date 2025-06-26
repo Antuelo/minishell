47e3ee14fa83ac731981b84c9923c5c86efd38f7
antuelo <antuel@outlook.com>
2025-06-26 12:01:23 +0200

    j'ai fait heredoc

--- src/execution/execute_utils.c
+++ src/execution/execute_utils.c
@@ -6,7 +6,7 @@
 /*   By: anoviedo <antuel@outlook.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 17:39:17 by anoviedo          #+#    #+#             */
-/*   Updated: 2025/06/26 08:56:49 by anoviedo         ###   ########.fr       */
+/*   Updated: 2025/06/26 11:59:50 by anoviedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	init_exec(t_exec *exec, int count)
 	exec->pipe_fd[1] = -1;
 	return (0);
 }
-
+/*
 static int	heredoc(char *delim)
 {
 	int		pipefd[2];
@@ -42,7 +42,7 @@ static int	heredoc(char *delim)
 		line = readline("> ");
 		if (!line)
 			break ;
-		if (ft_strncmp(line, delim, ft_strlen(line) + ft_strlen(delim)) == 0)
+		if (ft_strncmp(line, delim, ft_strlen(delim)) == 0)
 		{
 			free(line);
 			break ;
@@ -80,9 +80,9 @@ int	control_builtin(t_cmd *cmd_list, char ***envp)
 		return (1);
 	}
 	return (0);
-}
+}*/
 
-/*int	control_builtin(t_cmd *cmd_list, char ***envp)
+int	control_builtin(t_cmd *cmd_list, char ***envp)
 {
 	int	id;
 
@@ -96,7 +96,7 @@ int	control_builtin(t_cmd *cmd_list, char ***envp)
 		return (1);
 	}
 	return (0);
-}*/
+}
 
 int	control_infiles(t_cmd *cmd)
 {
